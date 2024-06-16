e4c505c6bf8559761dba5d1de888480c329b293b
TehnoTheDragon <tehnothedragon@gmail.com>
2024-06-16 22:49:40 +0300

    sandbox: playing with lexer

--- examples/sandbox/main.rs
+++ examples/sandbox/main.rs
@@ -1,3 +1,15 @@
+use ars::{lexer::{Lexer, LexerExt}, token::{Token, TokenData, TokenExt}};
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = vec![
+        Token::new_regex_from_str("whitespace", 0, "\\s+"),
+        Token::new_regex_from_str("number", 4, "\\d+(\\.\\d+)?"),
+        Token::new_lit("let", 1, "let"),
+        Token::new_regex_from_str("iden", 2, "[a-zA-Z_][a-zA-Z0-9_]*"),
+        Token::new_lit("equal", 3, "="),
+    ];
+    for tok in "let x = 10.5".tokenize_all(&pattern).iter() {
+        if tok.kind == 0 { continue; }
+        println!("{tok}");
+    }
 }
