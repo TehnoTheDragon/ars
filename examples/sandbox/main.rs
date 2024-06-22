8a973ab052fcdb3820a3758a9b1ba6c6a12c31e4
TehnoTheDragon <tehnothedragon@gmail.com>
2024-06-22 12:51:46 +0300

    visitor play

--- examples/sandbox/main.rs
+++ examples/sandbox/main.rs
@@ -1,36 +1,36 @@
-use std::vec;
+use std::{rc::Rc, vec};
 
-use ars::{ast::{Node, Value}, create_parser, lexer::Lexer, parser::ParserState, token::Token};
+use ars::{ast::{Node, Value}, create_parser, lexer::Lexer, parser::ParserState, token::Token, visitor::{Visitor, VisitorResult}};
 
 fn read_file(path: &str) -> String {
     std::fs::read_to_string(path).unwrap()
 }
 
-create_parser!(Register, 0, |_, state: &mut ParserState| {
+create_parser!(Register, 4, |_, state: &mut ParserState| {
     let register = state.eat();
-    Node::new(0, "Register", Value::String(register.value))
+    Node::new(4, "Register", Value::String(register.value))
 });
 
-create_parser!(Constant, 0, |_, state: &mut ParserState| {
+create_parser!(Constant, 3, |_, state: &mut ParserState| {
     let constant = state.eat();
-    Node::new(0, "Constant", Value::String(constant.value))
+    Node::new(3, "Constant", Value::String(constant.value))
 });
 
-create_parser!(Identity, 0, |_, state: &mut ParserState| {
+create_parser!(Identity, 2, |_, state: &mut ParserState| {
     match state.peek().kind {
         5 => { state.eat(); state.parse(Register) },
         7 => { state.eat(); state.parse(Constant) },
         _ => {
             let identity = state.require(vec![2]);
-            let node = Node::new(0, "Ident", Value::String(identity.value));
+            let node = Node::new(2, "Ident", Value::String(identity.value));
             node
         },
     }
 });
 
-create_parser!(Instruction, 0, |_, state: &mut ParserState| {
+create_parser!(Instruction, 1, |_, state: &mut ParserState| {
     let instruction = state.require(vec![1]);
-    let mut node = Node::new(0, "Instruction", Value::String(instruction.value));
+    let mut node = Node::new(1, "Instruction", Value::String(instruction.value));
     state.skip_until_found(vec![0]);
     node.add_child(state.parse(Identity));
     while state.is_kind(vec![4]) {
@@ -70,5 +70,24 @@ fn main() {
     lexer.begin(read_file("assets/tests/pseudo-assembly.pasm").as_str());
     let tokens = lexer.all();
     let mut parser = ParserState::new(tokens, Some(vec![0]));
-    println!("{}", parser.parse(Label));
+    let mut visitor = Visitor::new(None);
+    visitor.register(0, Box::new(|visitor, node| {
+        visitor.visit_children(&node.children)
+    }));
+    visitor.register(1, Box::new(|visitor, node| {
+        let mut compound = VisitorResult::Compound(vec![]);
+        for child in &node.children {
+            compound.add_child(visitor.visit_child(&child));
+        }
+        VisitorResult::Tagged("instruction".to_string(), Rc::new(compound))
+    }));
+    visitor.register(3, Box::new(|visitor, node| {
+        VisitorResult::Integer(node.value.as_string().parse::<u64>().unwrap())
+    }));
+    visitor.register(4, Box::new(|visitor, node| {
+        VisitorResult::Integer(node.value.as_string().parse::<u64>().unwrap())
+    }));
+
+    let result = visitor.visit(&parser.parse(Label));
+    print!("{}", result);
 }
