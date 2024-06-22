f2326e3327019e8cdc8efffb98bdda208e185311
TehnoTheDragon <tehnothedragon@gmail.com>
2024-06-22 12:51:38 +0300

    display for visitor result

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, collections::HashMap, sync::Arc};
+use std::{collections::HashMap, fmt::{format, Display}, ops::ShrAssign, rc::Rc, sync::Arc};
 
 use crate::ast::Node;
 
@@ -6,16 +6,79 @@ pub type VisitorFn = Box<dyn Fn(&Visitor, &Node) -> VisitorResult>;
 
 #[derive(Debug, Clone)]
 pub enum VisitorResult {
+    Compound(Vec<VisitorResult>),
     String(String),
+    Integer(u64),
+    Number(f64),
+    Tagged(String, Rc<VisitorResult>),
     None,
 }
 
 impl VisitorResult {
     pub fn as_string(&self) -> String {
         match self {
+            VisitorResult::Compound(s) => s.iter().map(|x| x.as_string()).collect(),
             VisitorResult::String(s) => s.to_string(),
+            VisitorResult::Integer(s) => s.to_string(),
+            VisitorResult::Number(s) => s.to_string(),
+            VisitorResult::Tagged(s, value) => format!("{s} {}", value.as_string()),
             VisitorResult::None => panic!("Value is None"),
         }
+    }
+
+    pub fn add_child(&mut self, child: VisitorResult) {
+        match self {
+            VisitorResult::Compound(s) => s.push(child),
+            _ => panic!("Value is not compound"),
+        }
+    }
+
+    pub fn display(&self, f: &mut std::fmt::Formatter<'_>, depth: usize) -> std::fmt::Result {
+        let line_color = format!("\x1b[38;2;{};{};{}m", 120, 120, 120);
+        let number_color = format!("\x1b[38;2;{};{};{}m", 240, 140, 30);
+        let label_color = format!("\x1b[38;2;{};{};{}m", 200, 30, 200);
+        let field_color = format!("\x1b[38;2;{};{};{}m", 150, 150, 170);
+        let string_color = format!("\x1b[38;2;{};{};{}m", 60, 180, 100);
+        let none_color = format!("\x1b[38;2;{};{};{}m", 255, 100, 100);
+        let stop = "\x1b[0m";
+
+        let space = " ".repeat(depth * 2);
+        match self {
+            VisitorResult::Compound(s) => {
+                write!(f, "{label_color}{}{stop}\n", "compound")?;
+                for (i, child) in s.iter().enumerate() {
+                    match child {
+                        VisitorResult::Tagged(_, _) => write!(f, "{space}{line_color}─{stop} ")?,
+                        VisitorResult::Compound(_) => write!(f, "{space}{line_color}─{stop} ")?,
+                        _ => {
+                            if i < s.len() - 1 {
+                                write!(f, "{space}{line_color}├─{stop} ")?;
+                            } else {
+                                write!(f, "{space}{line_color}└─{stop} ")?;
+                            }
+                        }
+                    }
+                    child.display(f, depth + 2)?;
+                }
+            }
+            VisitorResult::String(s) => write!(f, "{field_color}string:{stop} {string_color}{}{stop}\n", s)?,
+            VisitorResult::Integer(s) => write!(f, "{field_color}integer:{stop} {number_color}{}{stop}\n", s)?,
+            VisitorResult::Number(s) => write!(f, "{field_color}number:{stop} {number_color}{}{stop}\n", s)?,
+            VisitorResult::Tagged(s, value) => {
+                write!(f, "{label_color}{}{stop}\n", s)?;
+                write!(f, "{space}{line_color}└─{stop} ")?;
+                value.display(f, depth + 2)?;
+            }
+            VisitorResult::None => write!(f, "{none_color}none{stop}\n")?,
+        }
+
+        Ok(())
+    }
+}
+
+impl Display for VisitorResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.display(f, 0)
     }
 }
 
@@ -24,14 +87,20 @@ pub struct Visitor {
 }
 
 impl Visitor {
-    pub fn new(visitors: Vec<(u32, VisitorFn)>) -> Self {
+    pub fn new(visitors: Option<Vec<(u32, VisitorFn)>>) -> Self {
         let mut map = HashMap::new();
-        for (id, visitor) in visitors {
-            map.insert(id, visitor);
+        if let Some(visitors) = visitors {
+            for (kind, visitor) in visitors {
+                map.insert(kind, visitor);
+            }
         }
         Self {
             visitors: map,
         }
+    }
+
+    pub fn register(&mut self, kind: u32, visitor: VisitorFn) {
+        self.visitors.insert(kind, visitor);
     }
 
     pub fn visit(&self, node: &Node) -> VisitorResult {
@@ -49,9 +118,10 @@ impl Visitor {
     }
 
     pub fn visit_children(&self, children: &Vec<Arc<Node>>) -> VisitorResult {
+        let mut compound = VisitorResult::Compound(vec![]);
         for child in children {
-            self.visit_child(&child);
+            compound.add_child(self.visit_child(&child));
         }
-        VisitorResult::None
+        compound
     }
 }
